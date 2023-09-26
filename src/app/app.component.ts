9f1a4cabc02ae65f0dec851b41c16a1903d13323
Philipp Mössl <pmoessl@outlook.de>
2023-09-26 17:34:35 +0200

    change responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import AOS from "aos";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    AOS.init();
+
+  }
   
   constructor(public router: Router) {
 
